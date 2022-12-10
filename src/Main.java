653f608adab62a608d29ad5fcdb9182de6b48b46
OleksiiHillel <119528441+OleksiiHillel@users.noreply.github.com>
2022-12-10 12:54:49 +0200

    From lesson 28-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello Oleksii");
+
+        /*...*/
+
+        int a = 10;
+        int b = 3;
+
+        System.out.println(++a); //Сначала+1 потом показать
+        System.out.println(--b); //Сначало-1 потом показать
+
+        System.out.println(a++);
+        System.out.println(b--);
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println("мой текст" + a + "my second text");
+
+
     }
 }
