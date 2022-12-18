5230a4bda006d45cb265035a6eddc9a354a0dde3
OleksiiHillel <119528441+OleksiiHillel@users.noreply.github.com>
2022-12-18 18:19:03 +0200

    From Homework_26

--- src/Homework_26.java
+++ src/Homework_26.java
@@ -5,10 +5,41 @@ public class Homework_26 {
         int friendYear = 1992;
         System.out.println("Моему другу " + (currentYear-friendYear) + " лет");*/
 
- String number = "756848";
+ /*String number = "756848";
         System.out.println("number = " + number);
-        System.out.println("lastDigit будет = " + number.charAt(5));
+        System.out.println("lastDigit будет = " + number.charAt(5));*/
 
+        /*int age = 30;
+        String name = "Alex";
+        if(age >=18 && age<=30){
+            System.out.println(name + ",поздравляю вы разобрались как работает if");
+        }*/
+
+        /*int x = 34;
+        int y = 89;
+        if (x>0 && y>0 ){
+            System.out.println("Точка пересечения находится в 1-й плоскости");
+        }
+        if (x<0 && y>0 ){
+            System.out.println("Точка пересечения находится в 2-й плоскости");
+        }
+        if (x<0 && y<0 ){
+            System.out.println("Точка пересечения находится в 3-й плоскости");
+        }
+        if (x>0 && y<0 ){
+            System.out.println("Точка пересечения находится в 4-й плоскости");
+        }*/
+
+        int a = 4;
+        int b = 5;
+        int c = 3;
+
+        if ((a+b>c) && (b+c>a) && (a+c>b)){
+            System.out.println("Такой треугольник можно построить");
+        }
+        else {
+            System.out.println("Такой треугольник построить нельзя");
+        }
 
     }
 }
