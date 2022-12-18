8fb95609068aea2079f7c16720b502b5c379a065
OleksiiHillel <119528441+OleksiiHillel@users.noreply.github.com>
2022-12-18 17:14:57 +0200

    From Homework_26

---
+++ src/Homework_26.java
@@ -0,0 +1,14 @@
+public class Homework_26 {
+    public static void main(String[] args){
+
+        /*int currentYear = 2022;
+        int friendYear = 1992;
+        System.out.println("Моему другу " + (currentYear-friendYear) + " лет");*/
+
+ String number = "756848";
+        System.out.println("number = " + number);
+        System.out.println("lastDigit будет = " + number.charAt(5));
+
+
+    }
+}
