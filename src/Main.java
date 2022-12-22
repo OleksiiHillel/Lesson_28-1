e6507d6c4052b501f3744c5fb10a5d33b4a1799e
OleksiiHillel <119528441+OleksiiHillel@users.noreply.github.com>
2022-12-22 12:04:55 +0200

    From Homework_27

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,71 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Hello Oleksii");
 
         /*...*/
 
-        int a = 10;
-        int b = 3;
+        /*...*/
 
-        System.out.println(++a); //Сначала+1 потом показать
-        System.out.println(--b); //Сначало-1 потом показать
 
-        System.out.println(a++);
-        System.out.println(b--);
 
-        System.out.println(a);
-        System.out.println(b);
 
-        System.out.println("мой текст" + a + "my second text");
+        // s = console.nextLine();
 
+        //System.out.println("My string " + s );
+
+        /*while (true) {
+            String s_new = console.nextLine();
+            if (s_new.equals("exit")) {
+                break;
+            }
+        }*/
+
+        /*int i = 5;
+        do {
+            System.out.println(i);
+            i--;
+        } while (i > 0);
+
+        System.out.println("_________");
+        int l = 5;
+        l--;
+        while(l>0);
+        {
+            System.out.println(l);
+            l--;
+        }*/
+
+        /*int result = 0;
+        for (int i = 1; i<=5; i++){
+            if((i%2)==0){
+                System.out.println(i);
+
+                continue;
+
+            }
+            result = result +i;
+        }
+
+        Scanner console = new Scanner(System.in);
+        String s = console.nextLine();
+        int s_1 = Integer.parseInt(s);
+        System.out.println(s+1);
+        System.out.println(s_1+1);*/
+
+        System.out.println (My_first_method(maxNum));
 
     }
+
+    public static int My_first_method (int maxNum)
+    {
+        int result = 0;
+        for (int i = 0; i<= maxNum; i++);
+        {
+            result = result + i;
+        }
+        return result;
+    }
+
 }
